111b88a57b4fe9509502b8ee4635e6ae45c36629
Tjeerd <114989224+Tjeerd-171419@users.noreply.github.com>
2025-09-01 16:09:49 +0200

    animatie
    animatie en 3d effect

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,17 @@
-/* Stap 1: Selecteer de knop */
-let expandButton = document.querySelector('.profile-card-button')
+// /* Stap 1: Selecteer de knop */
+// let expandButton = document.querySelector('.profile-card-button')
 
-/* Stap 2: Wacht tot iemand op die knop klikt */
-expandButton?.addEventListener('click', function() {
+// /* Stap 2: Wacht tot iemand op die knop klikt */
+// expandButton?.addEventListener('click', function() {
 
-  /* Stap 3: Verander een class zodra iemand op die knop klikt */
-  document.body.classList.toggle('is-expanded')
-})
+//   /* Stap 3: Verander een class zodra iemand op die knop klikt */
+//   document.body.classList.toggle('is-expanded')
+// })
+
+// function toggleInfo() {
+//  document.getElementById("profile-info").style.display = "block";
+// }
+
+// function toggleInfo() {
+//  document.getElementById("profile-info").style.display = "none";
+// }
